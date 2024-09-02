c1f39d5e90a7941e8c1d2e22e9d3f063d5ec95ef
Sanjoy Saha <sanjoysaha0524@gmail.com>
2024-09-02 22:16:52 +0530

    recording?

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,16 +1,16 @@
-// import React from 'react'
+import React from 'react'
 
-// const Recordings = () => {
-//   return (
-//     <div className="flex flex-col items-start gap-2 xl:flex-row">
-//     <h1 className="text-base font-medium text-sky-1 lg:text-xl xl:min-w-32">
-//       Room
-//     </h1>
-//     <h1 className="truncate text-sm font-bold max-sm:max-w-[320px] lg:text-xl">
-//       {}
-//     </h1>
-//   </div>
-// );
-// }
+const Recordings = () => {
+  return (
+    <div className="flex flex-col items-start gap-2 xl:flex-row">
+    <h1 className="text-base font-medium text-sky-1 lg:text-xl xl:min-w-32">
+      Room
+    </h1>
+    <h1 className="truncate text-sm font-bold max-sm:max-w-[320px] lg:text-xl">
+      {}
+    </h1>
+  </div>
+);
+}
 
-// export default Recordings
+export default Recordings
